b0f353999a7c161be9832b9747dd376e03be788a
Kevin Jiang <jiang.kevin.studente@itispaleocapa.it>
2026-04-20 14:43:30 +0000

    versione nuova

---
+++ tep/aerei.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+
+
+int main(){
+    vector <vector<int>> v(3);
+    ifstream in("test.txt");
+    string s;
+    
+    
+    
+
+
+
+
+
+    return 0;
+}
