232ecae60a5af5a023ac69806b217fdbc69006aa
Werhon7 <cornali.werhonsebastian.studente@itispaleocapa.it>
2026-04-26 19:42:51 +0000

    vigenere

--- ParteIO/index.js
+++ ParteIO/index.js
@@ -12,8 +12,31 @@ function CriptoCesare(testo, key) {
     return testoRisultato;
 }
 
-function Cripto(btn,mittente,destinatario)
-{
+function Vigenere(car, key) {
+    var codiceOriginale = car.charCodeAt(0);
+    
+    var shift = key.toUpperCase().charCodeAt(0) - 65;
+    
+    shift %= 26; 
+    
+    var cripted = codiceOriginale + shift;
+    return String.fromCharCode(cripted);
+}
+
+function CriptoVigenere(testo, key) {
+    var testoRisultato = ""; 
+    
+    if (!key || key.length === 0) return testo;
+
+    for (let i = 0; i < testo.length; i++) {
+        var keyChar = key[i % key.length]; 
+        testoRisultato += Vigenere(testo[i], keyChar);
+    }
+    return testoRisultato;
+}
+
+
+function Cripto(btn, mittente, destinatario) {
     const Mittente = btn.dataset.mittente;
     const Testo = document.getElementById(Mittente).value;
     const BoxMittente = document.getElementById(mittente);
@@ -21,17 +44,18 @@ function Cripto(btn,mittente,destinatario)
     const BoxDestinatario = document.getElementById(destinatario);
 
     var TextCripto = Testo;
-    if(document.getElementById("CheckCesare").checked)
-    {
+
+    if (document.getElementById("CheckCesare").checked) {
         TextCripto = CriptoCesare(TextCripto, document.getElementById("KeyCesare").value);
     }
-    // if(document.getElementById("CheckVigenere").checked)
-    // {
+    
+    if (document.getElementById("CheckVigenere").checked) {
+        TextCripto = CriptoVigenere(TextCripto, document.getElementById("KeyVigenere").value);
+    }
 
-    // }
     BoxMittente.innerHTML += Testo + "</br>";
-    BoxCripto.innerHTML += TextCripto;
-    BoxDestinatario.innerHTML += Testo;
+    BoxCripto.innerHTML += TextCripto + "</br>"; 
+    BoxDestinatario.innerHTML += Testo + "</br>"; 
     
     document.getElementById(Mittente).value = "";
 }
