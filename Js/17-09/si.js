8b01dfe355f209fe608c3104d02b9d92720d8ef6
Werhon7 <cornali.werhonsebastian.studente@itispaleocapa.it>
2026-09-24 07:25:30 +0000

    si

--- Js/17-09/si.js
+++ Js/17-09/si.js
@@ -1,5 +1,12 @@
 var statoattuale = 0;
+var bool = true;
+var tempo;
 
+btn_pa = document.getElementById("btn-pa");
+btn_l = document.getElementById("btn-l");
+btn_r = document.getElementById("btn-r");
+
+// array di immagini
 const img_arr = [
     {
         nome: "image1",
@@ -29,33 +36,68 @@ const img_arr = [
     }
 ];
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const currentImage = img_arr[statoattuale];
-//         setTimeout(cambio, currentImage.tempo);
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    const immagine=img_arr[statoattuale];
+    document.getElementById("trulli").src="./img/" + immagine.nome + ".png";
+    document.body.style.backgroundColor=immagine.coloreBG;
+    
+    // Pianifica il primo cambio rispettando il tempo dell'immagine iniziale
+    tempo=setTimeout(cambio, immagine.tempo);
+});
 
-var bool = true;
+
 
 document.getElementById("trulli").addEventListener('click', () => {
-    bool = !bool;
+    bool=!bool;
+
+    if (!bool) 
+        clearTimeout(tempo);
+    else 
+        cambio();
+    
+});
+
+btn_pa.addEventListener('click', () => {
+    bool=!bool;
+
+    if(btn_pa.innerText=="⏸️")
+        btn_pa.innerText="▶️";
+    else
+        btn_pa.innerText="⏸️";
+    
+
+    if (!bool) 
+        clearTimeout(tempo);
+    else 
+        cambio();
+    
+});
+
+btn_l.addEventListener('click', () => {
+
+    if (bool) {
+        bool=!bool;
+        clearTimeout(tempo);}
     statoattuale--;
+    if (statoattuale < 0) {
+        statoattuale = img_arr.length - 1;
+    }
+
+    const immagine=img_arr[statoattuale];
+    document.getElementById("trulli").src="./img/"+immagine.nome+".png";
+    document.body.style.backgroundColor=immagine.coloreBG; 
+    
+});
+
+btn_r.addEventListener('click', () => {
+
+    if (bool) {
+        bool=!bool;
+        clearTimeout(tempo);}
     cambio();
-})
+    
+});
 
-// array di immagini
-
-
-
-
-
-// function cambio() {
-//     statoattuale++;
-//     if (statoattuale == 6) {
-//         statoattuale = 1;
-//     } 
-        
-//     document.getElementById("trulli").src = "./img/image" + statoattuale + ".png";
-// }
 
 
 function cambio() {
@@ -64,10 +106,17 @@ function cambio() {
         statoattuale = 0;
     }
 
-    const currentImage = img_arr[statoattuale];
-    document.getElementById("trulli").src = "./img/" + currentImage.nome + ".png";
-    document.body.style.backgroundColor = currentImage.coloreBG;
+    const immagine=img_arr[statoattuale];
+    document.getElementById("trulli").src="./img/"+immagine.nome+".png";
+    document.body.style.backgroundColor=immagine.coloreBG;
+
+    if (statoattuale == img_arr.length-1){
+        document.querySelector("h1").style.color = "white"
+        document.querySelector("p").style.color = "white";}
+    else{
+        document.querySelector("h1").style.color = "black";
+        document.querySelector("p").style.color = "black";}
 
     if (bool) 
-        setTimeout(cambio, currentImage.tempo);
+       tempo=setTimeout(cambio, immagine.tempo);
 }
